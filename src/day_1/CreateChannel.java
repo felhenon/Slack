5a7f62d6c1026c799f60efefcba4a6b5da6146ff
felhenon <huseyingursoy@hotmail.com>
2020-05-13 17:25:11 +0200

    createChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -2,6 +2,10 @@ package day_1;
 
 public class CreateChannel {
     public static void main(String[] args) {
-
+        System.out.println("user is able to create channels");
+        int x = 0;
+        if(x==0){
+            System.out.println("equal");
+        }
     }
 }
